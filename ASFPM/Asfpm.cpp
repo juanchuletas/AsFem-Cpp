4ae72dd707e89925879d1a3f59022814af4e5177
juanchuletas <gamj@xanum.uam.mx>
2021-09-20 20:05:00 -0500

    Exchange part

--- ASFPM/Asfpm.cpp
+++ ASFPM/Asfpm.cpp
@@ -4,37 +4,31 @@
 ASFPM::ASFPM(){
 }
 ASFPM::ASFPM(int _Ne, int _order,std::string _grid,int _atomicN,int _charge, int _angular,double _rN)// Pretty good constructor
-: FEMFP{_Ne,_order,"Fixed-Points",_grid},r0{0.0},atomicN{_atomicN},charge{_charge},angular{_angular},rN{_rN}{
+: FEMFP{_Ne,_order,"Fixed-Points",_grid},electronicStructure::closedShell{_atomicN,_charge},r0{0.0},
+    atomicN{_atomicN},charge{_charge},angular{_angular},rN{_rN}{
     std::cout<<"ASFEM constructor 1.\n";
+    
     total_nodes = globalSize;
     fem_nodes = bcSize;
     wfn = new double[fem_nodes*fem_nodes];
     eigenVal = new double[fem_nodes];
     rho = new double[fem_nodes];
-    //vr = new double[total_nodes];
 }
 ASFPM::~ASFPM(){
 
     delete [] wfn;
     delete [] eigenVal;
     delete [] rho;
-   // delete [] vr;
 }
 // SOME SETTERS AND GETTERS
-void ASFPM::setData(std::string _atomicModel){//Free-Atom Setter
-    atomicModel = _atomicModel;
-    occOrb = atomicN/2;
-    numElectrons = atomicN-charge;
-    totQ = numElectrons;
-
-}
-void ASFPM::setData(std::string _atomicModel,double rc, double wall=0.f){//Soft-Walls and hard Walls setter
-    atomicModel = _atomicModel;
+void ASFPM::setData(std::string _confine,double rc, double wall){//Soft-Walls and hard Walls setter
+    confType = _confine;
     cutRad = rc;
     wallValue = wall;
     occOrb = atomicN/2;
     numElectrons = atomicN-charge;
     totQ = numElectrons;
+    closedShell::setNumOfGridPoints(total_nodes);
 }
 // ********* PUPUPUPUPUPUPUPUPUPUBLIC METHODSSSS ************
 void ASFPM::wfnNormalization(){
@@ -77,6 +71,7 @@ double *ASFPM::getOrbitals(int numofOrb){
     for(int i=0; i<numofOrb; i++){
         for(int j=0; j<bcSize; j++){
             orbitals[k] = wfn[i*bcSize + j];
+            k++;
         }
     }
     return orbitals;
@@ -116,57 +111,120 @@ double ASFPM::computeTotalCharge(int rcIndex){
     delete [] totrho;
     delete [] feMatS;
     return finalq;
-}
-void ASFPM::getDensity(const double occup=2.0){
+} 
+void ASFPM::getDensity(const double occup){
 
     for(int i=0; i<bcSize; i++){
         rho[i] = 0.0;
         for(int orb=0; orb<occOrb; orb++){
             rho[i] += wfn[i + orb*bcSize]*wfn[i + orb*bcSize];
-            rho[i] = occup*rho[i];
+          
         }
+        rho[i] = occup*rho[i];
+    }
+}
+void ASFPM::getDensity(double *rhor){
+    for(int i=0; i<bcSize; i++){
+        rhor[i] = 0.0;
+        for(int orb=0; orb<occOrb; orb++){
+            rhor[i] += (wfn[i + orb*bcSize]*wfn[i + orb*bcSize]);  
+            //vrhor[i] = rhor[i]/femgrid[i+1];
+        }
+        rhor[i] = 2.0*rhor[i];
+        rhor[i] = rhor[i]/femgrid[i+1];
+        //printf("rho_r[%d] = %lf\n",i,rhor[i]);
     }
 }
 void ASFPM::getExchangeDens(double *rhox,int orb_a,int orb_b){
     for(int i=0; i<bcSize; i++){
         rhox[i] = 0.0;
         rhox[i] = wfn[orb_a*bcSize + i]*wfn[orb_b*bcSize + i];
+        rhox[i] = rhox[i]/femgrid[i+1];
     }
 }
 void ASFPM::getDensityMatrix(double *densMat){ //May be implemented in the Atomic Structure Static Class
     int k=0;
-    //std::cout<<"Orbital of Interest: "<<occOrb<<std::endl;
-    for(int i=0; i<bcSize; i++){
-        for(int j=0; j<bcSize; j++){
-            for(int orb=0; orb<occOrb; orb++){
-                densMat[k] = 2.0*(wfn[i + orb*bcSize]*wfn[j + orb*bcSize]);
+    for(int i=0; i<bcSize; i++)
+    {
+        for(int j=0; j<bcSize; j++)
+        {   densMat[k] = 0.0;
+            for(int orb=0; orb<occOrb; orb++)
+            {
+                densMat[k] += 2.0*(wfn[i + orb*bcSize]*wfn[j + orb*bcSize]);
             }
+            //densMat[k] = 2.0*densMat[k];
             k++;
         }
     }
 
 }
-double * ASFPM::computeHartreePotential(double *hpot){
-    double *vh_r = new double[total_nodes];
+// COULOMB TERM************************************
+double * ASFPM::hartreePotential(){
+    double *locdens = new double[bcSize];
+    double *hpot = new double[bcSize];
+    getDensity(locdens);
+    solvePoissonEquation(hpot,locdens,numElectrons-1);
+    delete []locdens;
 
+    return hpot;
+}
+double * ASFPM::computeHartreePotential(){
+    double *hpot = hartreePotential();
+
+    double *vh_r = new double[total_nodes];
     for(int i=0; i<bcSize; i++){
         vh_r[i+1]= hpot[i]/femgrid[i+1];
     }
     vh_r[0]=doInterpolation(&femgrid[0],vh_r,2);
-    vh_r[total_nodes-1]=totQ/femgrid[total_nodes-1];
+    vh_r[total_nodes-1]=(totQ-1)/femgrid[total_nodes-1];
+
+    delete [] hpot;
+
     return vh_r;
+
 }
-double * ASFPM::computeExchangePotential(double *hpot){
+// END COULOMB TERM************************************
+// EXCHANGE TERM *************************************
+double * ASFPM::exchangeAuxPotential(){
+    double *aux = new double[bcSize];
+    double *xdens = new double[bcSize];
+    double *uxpot = new double[bcSize];
+    for(int a=0; a<occOrb-1; a++){
+        for(int b=1; b<occOrb; b++){
+            if(a==b){
+                continue;
+            }
+            getExchangeDens(xdens,a,b);
+            solvePoissonEquation(aux,xdens,0.0);
+            for(int i=0; i<bcSize; i++){
+                uxpot[i] = 0.0;
+                uxpot[i] = uxpot[i] + aux[i];
+            }
+        }
+         
+    }
+    delete [] aux;
+    delete [] xdens;
+
+    return uxpot;
+}
+double * ASFPM::computeExchangePotential(){
+
+    double *uxpot = exchangeAuxPotential();
     double *vh_r = new double[total_nodes];
 
     for(int i=0; i<bcSize; i++){
-        vh_r[i+1]= hpot[i]/femgrid[i+1];
+        vh_r[i+1]= uxpot[i]/femgrid[i+1];
     }
     vh_r[0]=doInterpolation(&femgrid[0],vh_r,2);
     vh_r[total_nodes-1]=0.0;
+
+    delete [] uxpot;
     return vh_r;
 }
+//// END EXCHANGE TERM *************************************
 double * ASFPM::computeHartreePotential(double *hpot,int rcIndex){
+    
     double *vh_r = new double[total_nodes];
 
     for(int i=0; i<bcSize; i++){
@@ -186,20 +244,17 @@ double ASFPM::energyHF(double *hij, double *fij,double *densMat){
     double energy = 0.0;
     for(int i=0; i<bcSize*bcSize; i++)
     {
-        energy = energy + 0.5*(hij[i]*densMat[i]);
+        energy = energy + 0.5*(densMat[i]*hij[i]);
         energy = energy + 0.5*(fij[i]*densMat[i]);
     }
     return 0.5*energy;
 
 }
-void ASFPM::singleDiagonalization(){
+void ASFPM::singleDiagonalization(double *hij){
     std::cout<<"Single Diagonalization"<<std::endl;
-    double *hij = new double[bcSize*bcSize];
-    FillZeroMat(hij,bcSize,bcSize);
     SumMatrices(&vij[0],&kij[0],hij,bcSize*bcSize);
     diag(bcSize,hij,&sij[0],eigenVal,wfn);
-    printf("Lowest Occupied Orbital Energy: %.10lf\n",0.5*eigenVal[0]);
-    delete [] hij;
+    //printf("Lowest Occupied Orbital Energy: %.10lf\n",0.5*eigenVal[0]);
 }
 void ASFPM::load(){
     //Create the fem grid:
@@ -218,11 +273,53 @@ void ASFPM::load(){
     }
     
 }
+void ASFPM::sfc(double *fij, double *hij, double *vhpot, double *vxpot,double &energy0){
+    double *vhij = new double[bcSize*bcSize];
+    double *vxij = new double[bcSize*bcSize];
+    double *vtot = new double[bcSize*bcSize];
+    double *densMat = new double[bcSize*bcSize];
+    double *vXmat = new double[bcSize*bcSize];
+
+    int iter = 0;
+    double deltaEnergy = 100000000.0;
+    double new_HF = energy0;
+    double old_HF,orb_energy;
+    printf("-----------------------------------------------------------\n");
+    printf("-----------------------------------------------------------\n");
+    printf("-------------- Self-Consistent Field ----------------------\n");
+    printf("\n");
+   // while(deltaEnergy>0.0000001){
+    do{
+        old_HF = new_HF;
+        fixedPointsNumIntegration(vhij,vhpot);
+        //fixedPointsNumIntegration(vxij,vxpot);
+        //ScalarXMatrix(0.5,vxij,vXmat,bcSize,bcSize);
+        //SumMatrices(vhij,vXmat,vtot,bcSize*bcSize);
+        SumMatrices(hij,vhij,fij,bcSize*bcSize);
+        diag(bcSize,fij,&sij[0],eigenVal,wfn);
+        orb_energy = 0.5*eigenVal[0];
+        wfnNormalization();
+        getDensityMatrix(densMat);
+        vhpot = computeHartreePotential();
+        //vxpot = computeExchangePotential();
+        new_HF = energyHF(hij,fij,densMat);
+        printf("Hartree-Fock energy = %lf    orb = %lf   iteration: %d\n",new_HF,orb_energy,iter+1);
+          
+        deltaEnergy = fabs(new_HF-old_HF);
+        iter++;
+    }while(deltaEnergy>0.0000001);
+    delete [] vhij;
+    delete [] vxij;
+    delete [] vtot;
+    delete [] densMat;
+    delete []vXmat;
+
+}
 //*** METHODS TO START THE PROGRAM *********
 
 void ASFPM::startProgram(){
     //Checking initial data
-    if(atomicModel=="Soft-Walls"){
+    if(confType=="Soft-Walls"){
         if(atomicN!=1){
             double *uij{nullptr},*lij{nullptr};
             double *hij = new double[bcSize*bcSize];
@@ -351,14 +448,49 @@ void ASFPM::startProgram(){
             delete [] hij;
             }
     }
-    else if(atomicModel=="Hard-Walls"){
-
+    else if(confType=="Hard-Walls"){
+        printf("PUTO\n");
     }
     else{//FREE-ATOM
-
-
     }
 }
+void ASFPM::printWfn(int orbital){
+    int realOrb = orbital-1;
+    double wfnOrb;
+    int phase;
+    int phase2;
+    std::ofstream wfnData;
+    wfnData.open("wfn.dat",std::ios::out);
+    if(!wfnData){
+        std::cout<<"FILE NOT CREATED\n";
+    } 
+    getWfnPhase(fem_nodes,0,&phase,wfn);
+    getWfnPhase(fem_nodes,1,&phase2,wfn);
+    std::cout<<"The Wave Function Phase is: "<<phase<<std::endl;
+    wfnData<<"r \t orb[0] \t orb[1] "<<std::endl;
+    wfnData<<std::fixed<<std::setprecision(6)<<femgrid[0]<<"\t"<<0.0<<"\t"<<0.0<<std::endl;
+    for(int i=0; i<fem_nodes; i++)
+    {
+        wfnData<<std::fixed<<std::setprecision(6)<<femgrid[i+1]<<"\t"<<wfn[i + fem_nodes*0]<<"\t"<<wfn[i + fem_nodes*1]<<std::endl;
+
+    }
+    //wfnData<<std::fixed<<std::setprecision(6)<<femgrid[0]<<"\t"<<0.0<<"\t"<<0.0<<std::endl;
+    wfnData.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  /*    for(int i=0; i<total_nodes; i++){
     printf("Vr[%d] = %lf\n",i,vr[i]);
 } */
--- ASFPM/Asfpm.hpp
+++ ASFPM/Asfpm.hpp
@@ -1,46 +1,46 @@
 #if !defined(_ASFPM_H_)
 #define _ASFPM_H_
+#include <fstream>
 #include "../FEM_FP/FiniteElementFixedPoints.hpp" //Only add the 
 #include "../Tools/external_potential.hpp"
 #include "../Tools/matrices_operations.hpp"
 #include "../Tools/math_modules.hpp"
 #include "../ElectronicStructure/ElectronicStructure.hpp"
     
-class ASFPM : public FEMFP{
+class ASFPM : public FEMFP, public electronicStructure::closedShell {
 
     //This class performs the Finite Elemennt Method for Atomic Structure under the 
     // Fixed Points Model
     // CLASS MEMBERS
-    double r0;
-    double rN;
-    int atomicN,angular;
-    double cutRad, wallValue, lambda;
-    int total_nodes;
-    int fem_nodes;
-    int occOrb;
-    int charge;
-    int numElectrons; 
-    double totQ;
-    std::string atomName;
-    std::string atomicModel;
-    std::string confType;
-    std::string integrationScheme;
-    double *wfn{nullptr}, *eigenVal{nullptr},*rho{nullptr},*vr{nullptr}; 
+    
+        int occOrb;
+        double *wfn{nullptr}, *eigenVal{nullptr};
+        double r0;
+        double rN;
+        int atomicN,angular;
+        double cutRad, wallValue, lambda;
+        int total_nodes;
+        int fem_nodes;
+        int charge;
+        int numElectrons; 
+        double totQ;
+        std::string atomName;
+        std::string atomicModel;
+        std::string confType;
+        std::string integrationScheme;
+        double *rho{nullptr},*vr{nullptr}; 
     // VERY IMPORTANT MATRICES:
     //Matrix<double> sij, vij, kij;
     //double *uij{nullptr}, *lij{nullptr}; //Only when the poisson problem has a different size 
     //Private Methods
-    void getDensity(const double ocupation);
-    void getExchangeDens(double *rox, int a, int b);
-    void getExternalPotential(); //get the -z/r
-    void performSCF();
-    double* computeHartreePotential(double *hpot);
-    double* computeExchangePotential(double *xpot);
-    double* computeHartreePotential(double *hpot,int rcIndex);
-    void getExternalPotentialMatrix();
-    void wfnNormalization();
-    void getDensityMatrix(double *densMat);
-    double energyHF(double *hij, double *fij,double *densMat);
+        void getDensity(const double ocupation=2.0);
+        
+        void getExternalPotential(); //get the -z/r
+        void performSCF();
+        double* computeHartreePotential(double *hpot,int rcIndex);
+        void getExternalPotentialMatrix();
+    
+   
     double computeTotalCharge(int rcindex);
     //double integrateElement();
 
@@ -48,13 +48,24 @@ class ASFPM : public FEMFP{
         ASFPM();
         ASFPM(int _Ne, int _order,std::string _grid,int _atomicN,int _charge, int _angular,double _rN);
         ~ASFPM();
-        void setData(std::string _atomicmodel);
-        void setData(std::string _atomicmodel, double _rc, double _wall);
+        //void setData(std::string _atomicmodel);
+        void getExchangeDens(double *rox, int a, int b);
+        void setData(std::string _atomicmodel, double _rc=0.0, double _wall=0.0);
         void startProgram(); //Runs the program
         void load();
-        void singleDiagonalization();
+        void singleDiagonalization(double *hij);
         double *getOrbitals(int numOfOrb);
-
+        double *exchangeAuxPotential();
+        double *hartreePotential();
+        void getDensity(double *rhor);
+        void sfc(double *fij, double *hij, double *vhij, double *vxij,double &energy0);
+        double* computeHartreePotential();
+        double* computeExchangePotential();
+        void getDensityMatrix(double *densMat);
+        double energyHF(double *hij, double *fij,double *densMat);
+        void wfnNormalization();
+        void printWfn(int orb );
+        double computeBoundCond();
 };
 double integrateElement(int ei,int order,double *feMatS, int *link_mat, double coeff, double *cf);
 
